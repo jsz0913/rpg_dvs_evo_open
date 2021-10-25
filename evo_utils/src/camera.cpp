7fb71315d201287c17c10e0405fd3f6bc2943a07
jsz0913 <66623990+jsz0913@users.noreply.github.com>
2021-10-25 12:35:29 +0800

    Update camera.cpp

--- evo_utils/src/camera.cpp
+++ evo_utils/src/camera.cpp
@@ -8,21 +8,22 @@ void precomputeRectificationTable(
     static std::vector<cv::Point2f> points;
     points.clear();
     rectified_points.clear();
-
+    // fullResolution的类型
     cv::Size fr = cam.fullResolution();
 
     for (size_t y = 0; y != fr.height; ++y) {
         for (size_t x = 0; x != fr.width; ++x) {
+            // point是 x y
             points.push_back(cv::Point2f(x, y));
         }
     }
-
     const std::string distortion_type = cam.cameraInfo().distortion_model;
     LOG(INFO) << "Distortion type: " << distortion_type;
-
     if (distortion_type == "plumb_bob") {
+        // 重新标定的K
         auto K = cv::getOptimalNewCameraMatrix(cam.fullIntrinsicMatrix(),
                                                cam.distortionCoeffs(), fr, 0.);
+        // R P 分别使用单位阵 和 K
         cv::undistortPoints(points, rectified_points, cam.fullIntrinsicMatrix(),
                             cam.distortionCoeffs(), cv::Matx33f::eye(), K);
     } else {
@@ -33,17 +34,16 @@ void precomputeRectificationTable(
 
 image_geometry::PinholeCameraModel loadPinholeCamera(ros::NodeHandle& nh) {
     // Load camera calibration
+    // rpg_common_ros::param 得到 参数 
     const std::string camera_name =
         rpg_common_ros::param<std::string>(nh, "camera_name", "");
-
     const std::string calib_file =
         rpg_common_ros::param<std::string>(nh, "calib_file", "");
+    // file://
     const std::string url = std::string("file://") + calib_file;
-
     camera_info_manager::CameraInfoManager cam_info(nh, camera_name, url);
     image_geometry::PinholeCameraModel cam;
     cam.fromCameraInfo(cam_info.getCameraInfo());
-
     return cam;
 }
 }  // namespace evo_utils::camera
